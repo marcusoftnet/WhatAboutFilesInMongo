2f909bdb34742ec4b7c2b2d6ef5e675bd52df4c3
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-03-19 20:43:53 +0700

    And save the file is good

--- usingMongoDriver.js
+++ usingMongoDriver.js
@@ -82,11 +82,5 @@ module.exports.removeAllFiles = function (callback) {
     db.collection("fs.files").findAndRemove({}, function (err, number) {
       if(err){ return console.dir(err); }
     });
-
-    // And the same thing... but for the chunks... you remember?
-    // where mongo stores the data
-    db.collection("fs.chunks").findAndRemove({}, function (err, number) {
-      if(err){ return console.dir(err); }
-    });
   });
 };
